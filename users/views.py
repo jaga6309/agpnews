931e347beedd005ed9b9a541843f4b83a5f15906
dilipsapk <dilip.sapkota@simfoni.com>
2025-05-05 12:15:12 +0530

    updated login and logout functionality

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from users.models import Profile
 
 def user_register_view(request):
@@ -15,3 +16,17 @@ def user_register_view(request):
         usr = User.objects.create_user(username=usern, email=em, first_name=firstn, last_name=lastn, password=pswd)
         Profile.objects.create(mobile=mob, user=usr)
     return render(request, "users/user_register.html")
+
+def user_login_view(request):
+    if request.method=="POST":
+        data = request.POST
+        usern = data.get("uname")
+        pswd = data.get("psw")
+        user = authenticate(request, username=usern, password=pswd)
+        if user is not None:
+            login(request, user)
+    return render(request, "users/user_login.html")
+
+def user_logout_view(request):
+    logout(request)
+    return redirect("/users/login/")
